137756f8d50d1dcd7cfb0771982073e4de4e9345
kascice <unknown>
2011-01-13 16:05:45 +0000

    experiment

--- software/ncia-services-grid/src/gov/nih/nci/ncia/search/ImageSearchResultImpl.java
+++ software/ncia-services-grid/src/gov/nih/nci/ncia/search/ImageSearchResultImpl.java
@@ -2,18 +2,18 @@ package gov.nih.nci.ncia.search;
 
 /**
  * Represents a DICOM image (retrieved through drill down).
- * 
+ *
  * <P><b>WARNING!</b> This object is serialized so if you change it, you risk
  * breaking remote search and the grid interface
- * 
+ *
  * <p>Note: this class has a natural ordering that is inconsistent with equals.
  */
 public class ImageSearchResultImpl implements ImageSearchResult  {
 
 	public ImageSearchResultImpl() {
-		
+
 	}
-	
+
     /**
      * @return Returns the imagePkId.
      */
@@ -21,7 +21,7 @@ public class ImageSearchResultImpl implements ImageSearchResult  {
         return imagePkId;
     }
 
-    
+
     /**
      * @param imagePkId The imagePkId to set.
      */
@@ -32,7 +32,7 @@ public class ImageSearchResultImpl implements ImageSearchResult  {
 
     /**
      * Sort this by the instance number within the series.
-     * 
+     *
      * <p>Instance numbers aren't mandatory and can be null.  A null
      * instance number will be push the image to the front.
      */
@@ -50,8 +50,8 @@ public class ImageSearchResultImpl implements ImageSearchResult  {
 
         return instanceNumber.compareTo(otherInstanceNumber);
     }
-    
-    
+
+
     /**
      * {@inheritDoc}
      */
@@ -59,15 +59,15 @@ public class ImageSearchResultImpl implements ImageSearchResult  {
         return seriesPkId;
     }
 
-    
+
     /**
      * Set the series (pk) id for the series this image is in
-     */     
+     */
     public void setSeriesId(Integer seriesPkId) {
         this.seriesPkId = seriesPkId;
     }
 
-    
+
     /**
      * {@inheritDoc}
      */
@@ -75,7 +75,7 @@ public class ImageSearchResultImpl implements ImageSearchResult  {
         return instanceNumber;
     }
 
-    
+
     /**
      * Setter for instance number
      *
@@ -88,25 +88,25 @@ public class ImageSearchResultImpl implements ImageSearchResult  {
 
     /**
      * {@inheritDoc}
-     */    
-    public String getSeriesInstanceUid() {    	
+     */
+    public String getSeriesInstanceUid() {
     	return  this.seriesInstanceUid;
     }
 
-    
+
     /**
      * Set the DICOM series instance UID for the series this image is in
-     */    
+     */
     public void setSeriesInstanceUid(String seriesInstanceUid) {
         this.seriesInstanceUid = seriesInstanceUid;
     }
 
-    
+
     /**
      * {@inheritDoc}
-     */    
-    public String getSopInstanceUid() {    	
-    	return this.sopInstanceUid;    	
+     */
+    public String getSopInstanceUid() {
+    	return this.sopInstanceUid;
     }
 
     /**
@@ -116,10 +116,10 @@ public class ImageSearchResultImpl implements ImageSearchResult  {
         this.sopInstanceUid = sopInstanceUid;
     }
 
- 
+
     /**
      * {@inheritDoc}
-     */	
+     */
 	public Long getSize() {
 		return size;
 	}
@@ -132,7 +132,7 @@ public class ImageSearchResultImpl implements ImageSearchResult  {
 		this.size = size;
 	}
 
-	
+
     /**
      * {@inheritDoc}
      */
@@ -140,48 +140,48 @@ public class ImageSearchResultImpl implements ImageSearchResult  {
 		return location;
 	}
 
-	
+
     /**
      * {@inheritDoc}
-     */	
+     */
 	public void associateLocation(NBIANode location) {
 		this.location = location;
-	}  
-	
+	}
+
     /**
      * {@inheritDoc}
-     */	
+     */
     public String getThumbnailURL() {
     	return thumbnailURL;
     }
-	
+
 	/**
      * Set the URL to retrieve the thumbnail for this image result.
      */
 	public void setThumbnailURL(String thumbnailURL) {
 		this.thumbnailURL = thumbnailURL;
 	}
-	
-	public Integer getFrameNum() {
+
+	public Integer getFrameNum2() {
 		return frameNum;
 	}
 
-	public void setFrameNum(Integer frameSize) {
+	public void setFrameNum2(Integer frameSize) {
 		this.frameNum = frameSize;
 	}
-	
 
-	
-	
+
+
+
     ///////////////////////////////////////////////PRIVATE///////////////////////////////////
-	
+
     private Integer imagePkId;
     private Integer seriesPkId;
     private Integer instanceNumber;
     private String sopInstanceUid;
     private String seriesInstanceUid;
-    private Long size;   
-    private NBIANode location;  
+    private Long size;
+    private NBIANode location;
     private String thumbnailURL;
     private Integer frameNum;
 }

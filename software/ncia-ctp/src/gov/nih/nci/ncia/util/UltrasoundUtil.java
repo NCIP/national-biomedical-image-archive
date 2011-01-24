d332c3f6c5f8f953965ebe00868851bf36694d12
Jim Zhou <zhoujim@mail.nih.gov>
2011-01-24 19:52:16 +0000

    Use exception if the image is not Ultrasound image.

--- software/ncia-ctp/src/gov/nih/nci/ncia/util/UltrasoundUtil.java
+++ software/ncia-ctp/src/gov/nih/nci/ncia/util/UltrasoundUtil.java
@@ -13,7 +13,7 @@ public class UltrasoundUtil {
 
 public static final int[] MASKS = {0x0001, 0x0002, 0x0004, 0x0008, 0x0010, 0x0020,0x0040, 0x0100, 0x0200};
 
-	public static String getMultiModalityByCode(String code)
+	public static String getMultiModalityByCode(String code) throws Exception
 	{
 		NumberFormat nf = new DecimalFormat("#0000");
 	    int intCode = Integer.valueOf(code, 16);
@@ -31,7 +31,7 @@ public static final int[] MASKS = {0x0001, 0x0002, 0x0004, 0x0008, 0x0010, 0x002
 	}
 
 	public static void main(String[] args) {
-		String result = getMultiModalityByCode("0011");
+		String result = getMultiModalityByCode("Utltr");
 		System.out.println("result: " + result );
 	}
 }

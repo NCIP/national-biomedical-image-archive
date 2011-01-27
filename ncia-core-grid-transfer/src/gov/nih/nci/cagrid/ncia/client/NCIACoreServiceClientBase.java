e240d16bb86394020e699a120eeedb063f2177e6
kascice <unknown>
2011-01-27 20:40:43 +0000

    remove unused imports

--- ncia-core-grid-transfer/src/gov/nih/nci/cagrid/ncia/client/NCIACoreServiceClientBase.java
+++ ncia-core-grid-transfer/src/gov/nih/nci/cagrid/ncia/client/NCIACoreServiceClientBase.java
@@ -1,33 +1,19 @@
 package gov.nih.nci.cagrid.ncia.client;
 
+import gov.nih.nci.cagrid.introduce.security.client.ServiceSecurityClient;
+import gov.nih.nci.cagrid.ncia.stubs.NCIACoreServicePortType;
+import gov.nih.nci.cagrid.ncia.stubs.service.NCIACoreServiceAddressingLocator;
+
 import java.io.InputStream;
 import java.rmi.RemoteException;
 
-import javax.xml.namespace.QName;
-
-import java.util.Calendar;
-import java.util.List;
-
 import org.apache.axis.EngineConfiguration;
 import org.apache.axis.client.AxisClient;
-import org.apache.axis.client.Stub;
 import org.apache.axis.configuration.FileProvider;
 import org.apache.axis.message.addressing.EndpointReferenceType;
 import org.apache.axis.types.URI.MalformedURIException;
-
 import org.globus.gsi.GlobusCredential;
-
-import org.globus.wsrf.NotifyCallback;
 import org.globus.wsrf.NotificationConsumerManager;
-import org.globus.wsrf.container.ContainerException;
-
-import org.oasis.wsrf.lifetime.ImmediateResourceTermination;
-import org.oasis.wsrf.lifetime.WSResourceLifetimeServiceAddressingLocator;
-
-import gov.nih.nci.cagrid.ncia.stubs.NCIACoreServicePortType;
-import gov.nih.nci.cagrid.ncia.stubs.service.NCIACoreServiceAddressingLocator;
-import gov.nih.nci.cagrid.ncia.common.NCIACoreServiceI;
-import gov.nih.nci.cagrid.introduce.security.client.ServiceSecurityClient;
 
 
 /**

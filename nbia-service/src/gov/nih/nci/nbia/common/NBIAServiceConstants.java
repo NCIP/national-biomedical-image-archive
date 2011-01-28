1046d3e93c27a9da76f931801668dd9f73b010f2
kascice <unknown>
2011-01-28 17:54:58 +0000

    round 2.  couldnt figure out magic to get introduce to modify service to do remote api to cacore sdk so regenerating

---
+++ nbia-service/src/gov/nih/nci/nbia/common/NBIAServiceConstants.java
@@ -0,0 +1,13 @@
+package gov.nih.nci.nbia.common;
+
+import javax.xml.namespace.QName;
+
+
+/**
+ * Constants class that extends the introduce managed constants.  Developers can add constants to this file.
+ *
+ * @created by Introduce Toolkit version 1.4
+ */
+public interface NBIAServiceConstants extends NBIAServiceConstantsBase {
+	
+}
---
+++ nbia-service/src/gov/nih/nci/nbia/service/NBIAServiceImpl.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.nbia.service;
+
+import java.rmi.RemoteException;
+
+/** 
+ * TODO:I am the service side implementation class.  IMPLEMENT AND DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.4
+ * 
+ */
+public class NBIAServiceImpl extends NBIAServiceImplBase {
+
+	
+	public NBIAServiceImpl() throws RemoteException {
+		super();
+	}
+	
+}
+

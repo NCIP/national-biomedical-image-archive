81232f7cbf0b22f56083e766551c533e45e16c39
kascice <unknown>
2011-02-11 17:43:22 +0000

    fix values property

--- ncia-services-grid/src/gov/nih/nci/ncia/search/NameValuesPairs.java
+++ ncia-services-grid/src/gov/nih/nci/ncia/search/NameValuesPairs.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 /**
  * <P>This object should be considered immutable.... even though
  * it doesn't enforce that.
- * 
+ *
  * <P><b>WARNING!</b> This object is serialized so if you change it, you risk
  * breaking remote search and the grid interface
  */
@@ -16,7 +16,7 @@ public class NameValuesPairs  implements Serializable {
 
     /**
      * Gets the name value for this Model.
-     * 
+     *
      * @return name
      */
     public String getName() {
@@ -26,16 +26,16 @@ public class NameValuesPairs  implements Serializable {
 
     /**
      * Sets the name value for this Model.
-     * 
+     *
      * @param name
      */
     public void setName(String name) {
         this.name = name;
     }
-    
+
     /**
      * Gets the versions value for this Model.
-     * 
+     *
      * @return versions
      */
     public String[] getValues() {
@@ -45,32 +45,32 @@ public class NameValuesPairs  implements Serializable {
 
     /**
      * Sets the versions value for this Model.
-     * 
+     *
      * @param versions
      */
     public void setValues(String[] values) {
         this.values = values;
     }
-    
-    
+
+
     /**
      * This is necessary for the web services serializer to recognize
      * this property is an indexed property.
-     */      
-    public String getVersions(int i) {
+     */
+    public String getValues(int i) {
         return this.values[i];
     }
 
-    
+
     /**
      * This is necessary for the web services serializer to recognize
      * this property is an indexed property.
-     */      
-    public void setVersions(int i, String _value) {
+     */
+    public void setValues(int i, String _value) {
         this.values[i] = _value;
-    } 
-    
+    }
+
     ////////////////////////////////////////PRIVATE////////////////////////////////////
     private String name;
-    private String[] values;     
+    private String[] values;
 }

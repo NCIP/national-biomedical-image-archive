d79d1890a1c020c8621ed9912c975b21d0589036
kascice <unknown>
2011-02-18 03:35:05 +0000

    fix documentation and target patient

--- docs/useful_groovy_scripts/secure_grid_client_cql1.groovy
+++ docs/useful_groovy_scripts/secure_grid_client_cql1.groovy
@@ -6,43 +6,30 @@ import org.globus.gsi.GlobusCredential;
 import gov.nih.nci.cagrid.ncia.client.NCIACoreServiceClient;
 import org.globus.wsrf.encoding.ObjectDeserializer;
 
-/////////////////////////////////////////////////////////////////
-//NOTE: this script doesn't work because of XML parser collisions
-/////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+//NOTE: this script won't work unless you pull out javax/xml/transform 
+//      from jaxrpc.jar from Globus.  StreamSource classes with JAXP instance
+///////////////////////////////////////////////////////////////////////////////
 
-println System.getProperty("java.endorsed.dirs");
 
-def gridServiceUrl = "https://imaging-secure-dev.nci.nih.gov:8443/wsrf/services/cagrid/NCIACoreService";
 
-def gridUsername = "xxxx";
-def gridPassword = "xxxxx.";
-def authUrl = "https://cagrid-auth-stage.nci.nih.gov:8443/wsrf/services/cagrid/AuthenticationService";
-def dorianURL = "https://cagrid-dorian-stage.nci.nih.gov:8443/wsrf/services/cagrid/Dorian";
+def gridServiceUrl = "https://imaging-secure.nci.nih.gov:8443/wsrf/services/cagrid/NCIACoreService";
+
+def gridUsername = "xxxxx";
+def gridPassword = "xxxxx";
+def authUrl = "https://cagrid-auth.nci.nih.gov:8443/wsrf/services/cagrid/AuthenticationService";
+def dorianURL = "https://cagrid-dorian.nci.nih.gov:8443/wsrf/services/cagrid/Dorian";
 	
-	
-/*String cql = 
-"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
-"<ns1:CQLQuery xmlns:ns1=\"http://CQL.caBIG/1/gov.nih.nci.cagrid.CQLQuery\">"+
-"  <ns1:Target name=\"gov.nih.nci.ncia.domain.Image\""+
-"              xsi:type=\"ns1:Object\""+
-"              xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"+
-"    <ns1:Attribute name=\"sopInstanceUID\""+
-"                   predicate=\"EQUAL_TO\""+
-"                   value=\"1.1.33844956.480\""+
-"                   xsi:type=\"ns1:Attribute\"/>"+
-"</ns1:Target>"+
-"</ns1:CQLQuery>";
-*/
 	
 String cql = 
 "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
 "<ns1:CQLQuery xmlns:ns1=\"http://CQL.caBIG/1/gov.nih.nci.cagrid.CQLQuery\">"+
-"  <ns1:Target name=\"gov.nih.nci.ncia.domain.TrialDataProvenance\""+
+"  <ns1:Target name=\"gov.nih.nci.ncia.domain.Patient\""+
 "              xsi:type=\"ns1:Object\""+
 "              xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"+
-"    <ns1:Attribute name=\"project\""+
+"    <ns1:Attribute name=\"id\""+
 "                   predicate=\"EQUAL_TO\""+
-"                   value=\"TCGA\""+
+"                   value=\"1\""+
 "                   xsi:type=\"ns1:Attribute\"/>"+
 "</ns1:Target>"+
 "</ns1:CQLQuery>";
@@ -57,7 +44,7 @@ try {
 																															 authUrl);        
 		println("identity:"+globusCred.getIdentity());		
 
-		def nciaCoreServiceClient = new NCIACoreServiceClient(gridServiceUrl, globusCred*);
+		def nciaCoreServiceClient = new NCIACoreServiceClient(gridServiceUrl, globusCred);
 		nciaCoreServiceClient.setAnonymousPrefered(false);
 
 

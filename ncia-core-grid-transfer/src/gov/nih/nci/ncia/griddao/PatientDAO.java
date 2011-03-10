4582bf37a12ca91726b0c709d442f164680644a3
kascice <unknown>
2011-03-10 20:39:34 +0000

    remove unused import

--- ncia-core-grid-transfer/src/gov/nih/nci/ncia/griddao/PatientDAO.java
+++ ncia-core-grid-transfer/src/gov/nih/nci/ncia/griddao/PatientDAO.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.springframework.dao.DataAccessException;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.transaction.annotation.Propagation;
@@ -17,12 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 /**
  * This object does hand-rolled data access for anything patient related in the
- * grid service.  
- * 
+ * grid service.
+ *
  * <p>This came from a massive object called ImageFileProcessor which was split
  * into this object and several other "DAO" objects to isolate data access according
  * to similar pattern as rest of system.
- * 
+ *
  * @author lethai
  */
 public class PatientDAO extends HibernateDaoSupport implements PatientDAOInterface{
@@ -39,7 +38,7 @@ public class PatientDAO extends HibernateDaoSupport implements PatientDAOInterfa
 					" WHERE p.id=s.patient.id AND p.id=gs.patientPkId AND p.patientId='" +
 					patientId + "' AND s.studyDate IS NOT NULL AND gs.visibility='1' " +
 					" ORDER BY s.studyDate";
-		
+
 		List<Date> studyDates = new ArrayList<Date>();
 
 		if (patientId == null ) {
@@ -48,8 +47,8 @@ public class PatientDAO extends HibernateDaoSupport implements PatientDAOInterfa
 		List<Study> rs = this.getHibernateTemplate().find(hql);
 		for (Study s : rs) {
 			Date sd = s.getStudyDate();
-			if(sd != null && !studyDates.contains(sd)){						
-					studyDates.add(sd);						
+			if(sd != null && !studyDates.contains(sd)){
+					studyDates.add(sd);
 			}
 		}
 		if(studyDates.isEmpty()){
@@ -58,5 +57,5 @@ public class PatientDAO extends HibernateDaoSupport implements PatientDAOInterfa
 
 		return studyDates;
 	}
-	
+
 }

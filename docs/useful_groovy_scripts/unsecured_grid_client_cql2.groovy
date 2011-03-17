00403cb0e38e1109a620ed62a2b3f893ff1bd182
kascice <unknown>
2011-03-17 18:38:22 +0000

    add fetch

--- docs/useful_groovy_scripts/unsecured_grid_client_cql2.groovy
+++ docs/useful_groovy_scripts/unsecured_grid_client_cql2.groovy
@@ -2,23 +2,31 @@ import org.cagrid.cql2.CQLQuery;
 import org.cagrid.cql.utilities.CQL2SerializationUtil;
 import org.cagrid.cql2.results.CQLQueryResults;
 import gov.nih.nci.nbia.client.NBIAServiceClient;
+import groovy.xml.MarkupBuilder
 
-
-def gridServiceUrl = "http://xxxx:xxxxx/wsrf/services/cagrid/NCIACoreService"
+def gridServiceUrl = "http://nciavgriddev5004.nci.nih.gov:39380/wsrf/services/cagrid/NBIAService"
 
 
 
 def cql2Writer = new StringWriter()
 def cql2Builder = new MarkupBuilder(cql2Writer)
-cql2Builder.'ns1:CQLQuery'('xmlns:ns1': 'http://CQL.caBIG/2/org.cagrid.cql2') {
-  'ns1:CQLTargetObject'(className:'gov.nih.nci.ncia.domain.TrialDataProvenance') {
-    'ns1:CQLAttribute'(name:'project') {
+cql2Builder.
+'ns1:CQLQuery'('xmlns:ns1': 'http://CQL.caBIG/2/org.cagrid.cql2') {
+  'ns1:CQLTargetObject'(className:'gov.nih.nci.ncia.domain.Patient') {
+    'ns1:CQLAttribute'(name:'id') {
       'ns1:BinaryPredicate'('EQUAL_TO')
       'ns1:AttributeValue' {
-        'ns1:StringValue'('TCGA')
+        'ns1:IntegerValue'('1')
       }
     }  
   }
+  
+  'ns1:AssociationPopulationSpecification' {
+    'ns1:NamedAssociationList' {
+      'ns1:NamedAssociation'(endName:'studyCollection')
+    }
+  }
+  
 }
 
 

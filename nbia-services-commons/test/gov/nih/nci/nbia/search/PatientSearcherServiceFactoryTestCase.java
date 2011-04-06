9448eaa6a8b593f7e7d608b03ac01cc304c7a1eb
kascice <unknown>
2011-04-06 21:10:03 +0000

    ncia->nbia

--- nbia-services-commons/test/gov/nih/nci/nbia/search/PatientSearcherServiceFactoryTestCase.java
+++ nbia-services-commons/test/gov/nih/nci/nbia/search/PatientSearcherServiceFactoryTestCase.java
@@ -1,6 +1,7 @@
 package gov.nih.nci.nbia.search;
 
 import gov.nih.nci.nbia.query.DICOMQuery;
+import gov.nih.nci.ncia.search.NBIANode;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;

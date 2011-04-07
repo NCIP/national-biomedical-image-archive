a26469939b2964b59876fdd029a2ece6bfec8dd9
kascice <unknown>
2011-04-07 13:15:31 +0000

    ncia->nbia

--- software/nbia-services-commons/test/gov/nih/nci/nbia/search/ThumbnailResolverFactoryTestCase.java
+++ software/nbia-services-commons/test/gov/nih/nci/nbia/search/ThumbnailResolverFactoryTestCase.java
@@ -17,22 +17,22 @@ public class ThumbnailResolverFactoryTestCase extends TestCase {
 		}
 
 	}
-	
+
 	public void testGetThumbnailURLResolver() {
 		System.setProperty("thumbnailResolver.className",
-				           "gov.nih.nci.ncia.search.ThumbnailResolverFactoryTestCase$FakeThumbnailURLResolver");
-		
+				           "gov.nih.nci.nbia.search.ThumbnailResolverFactoryTestCase$FakeThumbnailURLResolver");
+
 		ThumbnailURLResolver resolver = ThumbnailResolverFactory.getThumbnailURLResolver();
 		ThumbnailURLResolver resolver2 = ThumbnailResolverFactory.getThumbnailURLResolver();
 
 		assertEquals(resolver, resolver2);
 		assertTrue(resolver instanceof FakeThumbnailURLResolver);
-	}	
-	
+	}
+
 	public static class FakeThumbnailURLResolver implements ThumbnailURLResolver {
 		/**
-		 * For a given local DICOM image, return a URL (String) to get to a thumbnail of it.	 
-		 */	
+		 * For a given local DICOM image, return a URL (String) to get to a thumbnail of it.
+		 */
 		public String resolveThumbnailUrl(ImageDTO imageDto) {
 			return "foo";
 		}

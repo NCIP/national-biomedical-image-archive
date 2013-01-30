1288bbb4993f2ec0596f20e8810115a13ba5c18d
Qinyan Pan <panq@mail.nih.gov>
2013-01-30 21:32:38 +0000

    For showing Cineloop for US Image.

--- nbia-web/webapp/js/imageViewer.js
+++ nbia-web/webapp/js/imageViewer.js
@@ -7,3 +7,9 @@ function showInNewWindow(imagename)
 	                     config='height=500,width=500, toolbar=no, menubar=no, scrollbars=yes, resizable=yes,location=no, directories=no, status=no');
 	popup.focus();
 }
+
+function setHiddenValue(imageLabel, slideshowtemplate){
+	document.getElementById('MAINbody:imageForm:imageIdx').value=imageLabel; 
+	document.getElementById('MAINbody:imageForm').submit();   
+	window.open(slideshowtemplate, 'slideshow', 'resizable=no,scrollbars=no,width=602,height=602');
+}

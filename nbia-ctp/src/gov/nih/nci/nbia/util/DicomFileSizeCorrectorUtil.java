ff53ab74176717dc7193f6ad736f62a7830e13ff
Vrushali Nikte <vrushali.nikte@nih.gov>
2013-02-13 20:24:40 +0000

    Implemented NBIA-517- Update DICOM File Sizes in Database

---
+++ nbia-ctp/src/gov/nih/nci/nbia/util/DicomFileSizeCorrectorUtil.java
@@ -0,0 +1,158 @@
+package gov.nih.nci.nbia.util;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import org.rsna.ctp.objects.DicomObject;
+
+public final class DicomFileSizeCorrectorUtil {
+
+	private String connectionUrl;
+	private String driverClass;
+	private String userName;
+	private String password;
+	
+	public String getConnectionUrl() {
+		return connectionUrl;
+	}
+	public void setConnectionUrl(String connectionUrl) {
+		this.connectionUrl = connectionUrl;
+	}
+	public String getDriverClass() {
+		return driverClass;
+	}
+	public void setDriverClass(String driverClass) {
+		this.driverClass = driverClass;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String mysqlDriverClass = "com.mysql.jdbc.Driver";
+		String oracleDriverClass = "oracle.jdbc.driver.OracleDriver";
+		
+		String mysqlConnectionUrl = "jdbc:mysql://";
+		String oracleConnectionUrl = "jdbc:oracle:thin:@";
+
+		String uIDatabaseType = getUserInput("Enter Database Type (mysql Or oracle): ");
+		String uIDatabaseServer = getUserInput("Enter Database Server Name (localhost): ");
+		String uIDatabasePort = getUserInput("Enter Database Port (3306 Or 1521): ");
+		String uIDatabaseName = getUserInput("Enter Database Name (nbiadb Or nbiadev): ");
+
+		String databaseConnectionUrl = uIDatabaseServer +":"+uIDatabasePort+"/"+ uIDatabaseName;
+		
+		String userInputUserName = getUserInput("Enter Database User: ");
+		String userInputPassword = getUserInput("Enter Database Password: ");
+		
+		DicomFileSizeCorrectorUtil sizeCorrector = new DicomFileSizeCorrectorUtil();
+		
+		if (uIDatabaseType.equalsIgnoreCase("mysql")) {
+			sizeCorrector.setConnectionUrl(mysqlConnectionUrl + databaseConnectionUrl);
+			sizeCorrector.setDriverClass(mysqlDriverClass);
+		} else if(uIDatabaseType.equalsIgnoreCase("oracle")) {
+			sizeCorrector.setConnectionUrl(oracleConnectionUrl + databaseConnectionUrl);
+			sizeCorrector.setDriverClass(oracleDriverClass);
+		} else {
+			System.out.println("Please enter valid database type. ");
+			return;
+		}
+		sizeCorrector.setUserName(userInputUserName);
+		sizeCorrector.setPassword(userInputPassword);
+		sizeCorrector.runFileSizeCorrector();
+	}
+	public static String getUserInput(String msg) {
+		//  prompt the user to enter 
+	    System.out.print(msg);
+	    // get their input
+	    Scanner scanner = new Scanner(System.in);
+	    String userInput = scanner.nextLine();
+	    // test the String if empty then prompt again
+	    if (userInput.trim().equals("")) {
+		   return getUserInput(msg);
+		} else {
+			return userInput.trim();
+		}
+	}
+	private Connection getJDBCConnection() throws ClassNotFoundException, SQLException {
+		// Load the JDBC-ODBC bridge
+		Class.forName(driverClass);
+		// specify the ODBC data source's URL
+		return DriverManager.getConnection(connectionUrl, userName, password);
+	}
+	public void runFileSizeCorrector() {
+		try {
+			Connection con = getJDBCConnection();
+			con.setAutoCommit(false);
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt
+					.executeQuery("Select image_pk_id,dicom_file_uri,dicom_size from general_image order by image_pk_id asc");
+
+			while(rs.next()){				
+				 String imageId = rs.getString(1);
+				 String storedFileName = rs.getString(2);
+				 long storedFileSize = rs.getLong(3);
+				 File storedFile = new File(storedFileName);
+				 if (!storedFile.exists()) {
+					System.out.println("Current file Name: " + storedFileName
+							+ " was not found...");
+				 } else if(storedFileSize != storedFile.length()) {
+					 System.out.println("File size mismatch####### "+imageId+ "|| "+ storedFileName + " || " + storedFileSize);
+					setCorrectFileSize(con, storedFile,imageId );
+				 } else {
+					 //Do nothing
+					 //System.out.println("file size is same.....");
+				 }
+			}
+			// close statement and connection
+			stmt.close();
+			con.close();
+		} catch (java.lang.Exception ex) {
+			System.out.println("Error occured" + ex.getMessage());
+		}
+		System.out.println("DICOM File size correction has been completed....");
+	}
+	 private void setCorrectFileSize(Connection con, File file, String imageId) throws SQLException {
+    	 // Temporary fix until new CTP release provides a better solution
+    	long actualFileSize = file.length();
+    	
+    	try {
+    		DicomObject tempFile = new DicomObject(file);
+    		String md5 = tempFile.getDigest()== null? " " : tempFile.getDigest();
+    		System.out.println("updated filesize " + actualFileSize + "updated md5 " + md5);
+
+    		String sql ="UPDATE general_image SET dicom_size =? , md5_digest=? where image_pk_id=?";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setBigDecimal(1, BigDecimal.valueOf(actualFileSize));
+			stmt.setString(2, md5);
+			stmt.setString(3, imageId);
+    		stmt.execute() ;
+			// close statement 
+    		con.commit();
+			stmt.close();
+			
+		} catch (Exception ex) {
+    		System.out.println("update failed for imageId "+ imageId +" exception is:- " + ex.getMessage());
+    		con.rollback();
+    	}
+    	
+    }
+}

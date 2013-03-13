b58eaab4039c8a45b8c83c1af96f811f195a6aa7
Vrushali Nikte <vrushali.nikte@nih.gov>
2013-03-13 15:59:24 +0000

    Fixed NBIA-515

--- nbia-remote-search/src/gov/nih/nci/nbia/dicomtags/RemoteDicomTagViewer.java
+++ nbia-remote-search/src/gov/nih/nci/nbia/dicomtags/RemoteDicomTagViewer.java
@@ -6,8 +6,11 @@ import gov.nih.nci.ncia.dto.DicomTagDTO;
 import gov.nih.nci.ncia.search.ImageSearchResult;
 import gov.nih.nci.ncia.search.ImageSearchResultImpl;
 
+import java.rmi.RemoteException;
 import java.util.Arrays;
 import java.util.List;
+
+import org.apache.axis.types.URI.MalformedURIException;
 
 /**
  * Implementation of DicomTagViewer that can retrieve the tags
@@ -23,30 +26,49 @@ public class RemoteDicomTagViewer implements DicomTagViewer {
 	public List<DicomTagDTO> viewDicomHeader(ImageSearchResult imageSearchResult) {
 		RemoteNode remoteNode = (RemoteNode)imageSearchResult.associatedLocation();
 		String serviceAddress = remoteNode.getEndpointReferenceType().getAddress().toString();
-		String remoteNodeVersion = remoteNode.getServiceMetadata().getServiceDescription().getService().getVersion();
-		System.out.println("remoteNode Version" + remoteNodeVersion);
+		NCIACoreServiceClient nbiaServiceClient;
 		try {
-			NCIACoreServiceClient nbiaServiceClient = new NCIACoreServiceClient(serviceAddress);
+			nbiaServiceClient = new NCIACoreServiceClient(serviceAddress);
 			// TODO: KEEP ONLY "else" code when all nodes (remote and local) moves to NBIA 5.1 then below code is not needed. 
 			RemoteNode location = RemoteNode.constructPartialRemoteNode(remoteNode.getDisplayName(),remoteNode.getURL());
 			imageSearchResult.associateLocation(location);
-			if (remoteNodeVersion != null && remoteNodeVersion.equals("1.3")) {
-				ImageSearchResultImpl imageSearchImpl = new ImageSearchResultImpl();
-				imageSearchImpl.setId(imageSearchResult.getId());
-				imageSearchImpl.setInstanceNumber(imageSearchResult.getInstanceNumber());
-				imageSearchImpl.setSeriesId(imageSearchResult.getSeriesId());
-				imageSearchImpl.setSeriesInstanceUid(imageSearchResult.getSeriesInstanceUid());
-				imageSearchImpl.setSize(imageSearchResult.getSize());
-				imageSearchImpl.setSopInstanceUid(imageSearchResult.getSopInstanceUid());
-				imageSearchImpl.setThumbnailURL(imageSearchResult.getThumbnailURL());
-				return Arrays.asList(nbiaServiceClient.viewDicomHeader(imageSearchImpl));	
+			if ( remoteNode.getServiceMetadata().getServiceDescription() != null && remoteNode.getServiceMetadata().getServiceDescription().getService().getVersion() != null 
+					&& remoteNode.getServiceMetadata().getServiceDescription().getService().getVersion().equals("1.3")) {
+				return getDicomHeaderForV13(imageSearchResult, serviceAddress);	
 			} else {
 				return Arrays.asList(nbiaServiceClient.viewDicomHeader(imageSearchResult));
 			}
 		}
 		catch(Exception ex) {
 			ex.printStackTrace();
-			throw new RuntimeException(ex);
+			try {
+				//TODO: remove the try once all remote nodes moves to NBIA 5.1
+				// this exception will come mostly when querying remote node(version 1.3)
+				return getDicomHeaderForV13(imageSearchResult, serviceAddress);
+			} catch (MalformedURIException e) {
+				e.printStackTrace();
+				throw new RuntimeException(ex);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+				throw new RuntimeException(ex);
+			}
+			
 		}
+	}
+
+	private List<DicomTagDTO> getDicomHeaderForV13(ImageSearchResult imageSearchResult,
+			String serviceAddress) throws MalformedURIException,
+			RemoteException {
+		NCIACoreServiceClient nbiaServiceClient;
+		nbiaServiceClient = new NCIACoreServiceClient(serviceAddress);
+		ImageSearchResultImpl imageSearchImpl = new ImageSearchResultImpl();
+		imageSearchImpl.setId(imageSearchResult.getId());
+		imageSearchImpl.setInstanceNumber(imageSearchResult.getInstanceNumber());
+		imageSearchImpl.setSeriesId(imageSearchResult.getSeriesId());
+		imageSearchImpl.setSeriesInstanceUid(imageSearchResult.getSeriesInstanceUid());
+		imageSearchImpl.setSize(imageSearchResult.getSize());
+		imageSearchImpl.setSopInstanceUid(imageSearchResult.getSopInstanceUid());
+		imageSearchImpl.setThumbnailURL(imageSearchResult.getThumbnailURL());
+		return Arrays.asList(nbiaServiceClient.viewDicomHeader(imageSearchImpl));
 	}
 }

88608273a3ece7d7182d9e6bbf6476af2202a189
Vrushali Nikte <vrushali.nikte@nih.gov>
2013-03-26 19:29:06 +0000

    NBIA-527 Added validation for image type 3,Scanning Sequence and Scanning Variant

--- nbia-ctp/src/gov/nih/nci/nbia/domain/operation/MRImageOperation.java
+++ nbia-ctp/src/gov/nih/nci/nbia/domain/operation/MRImageOperation.java
@@ -3,8 +3,8 @@
  */
 package gov.nih.nci.nbia.domain.operation;
 
-import gov.nih.nci.nbia.internaldomain.MRImage;
 import gov.nih.nci.nbia.internaldomain.GeneralImage;
+import gov.nih.nci.nbia.internaldomain.MRImage;
 import gov.nih.nci.nbia.util.DicomConstants;
 import gov.nih.nci.nbia.util.SpringApplicationContext;
 
@@ -17,103 +17,105 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  *
  */
-public class MRImageOperation extends DomainOperation implements MRImageOperationInterface{
-	
-	private GeneralImage gi;
+public class MRImageOperation extends DomainOperation implements
+        MRImageOperationInterface {
 
-	public MRImageOperation() {
-	}
-	
-	@Transactional(propagation=Propagation.REQUIRED)
-	public Object validate(Map numbers) throws Exception {
-		
-		MRImage mri = (MRImage)SpringApplicationContext.getBean("mrimage");
-		
-		try {
-	        String hql = "from MRImage as image where ";        
-	
-	        hql += (" image.generalImage.id = " + this.gi.getId()); //general_image pk_id
-	
-	        mri.setGeneralImage(gi);
-	        mri.setGeneralSeries(gi.getGeneralSeries());
-	        
-	        //the update method should be changed to search
-	       // mri = (MRImage) this.update(hql, mri);
-	        List ret = getHibernateTemplate().find(hql);
-	        if(ret != null && ret.size() > 0) {
-	        	if(ret.size() == 1) {
-	        		mri = (MRImage)ret.get(0);
-	        	}else if (ret.size() > 1){
-	        		throw new Exception("mr_image table has duplicate records, please contact Data Team to fix data, then upload data again");
-	        	}
-	        }
-	
-	        populateMRImageFromNumbers(numbers, mri);
+    private GeneralImage gi;
 
-		}catch(Exception e) {
-			//log.error("Exception in MRImageOperation " + e);
-			throw new Exception("Exception in MRImageOperation " + e);
-		}
+    public MRImageOperation() {
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Object validate(Map numbers) throws Exception {
+
+        MRImage mri = (MRImage) SpringApplicationContext.getBean("mrimage");
+
+        try {
+            String hql = "from MRImage as image where ";
+
+            hql += (" image.generalImage.id = " + this.gi.getId()); // general_image
+                                                                    // pk_id
+
+            mri.setGeneralImage(gi);
+            mri.setGeneralSeries(gi.getGeneralSeries());
+
+            // the update method should be changed to search
+            // mri = (MRImage) this.update(hql, mri);
+            List ret = getHibernateTemplate().find(hql);
+            if (ret != null && ret.size() > 0) {
+                if (ret.size() == 1) {
+                    mri = (MRImage) ret.get(0);
+                } else if (ret.size() > 1) {
+                    throw new Exception(
+                            "mr_image table has duplicate records, please contact Data Team to fix data, then upload data again");
+                }
+            }
+
+            populateMRImageFromNumbers(numbers, mri);
+
+        } catch (Exception e) {
+            // log.error("Exception in MRImageOperation " + e);
+            throw new Exception("Exception in MRImageOperation " + e);
+        }
 
         return mri;
-		
-	}
-	
-	public void setGeneralImage(GeneralImage gi) {
-		this.gi = gi;
-	}
 
-	
+    }
+
+    public void setGeneralImage(GeneralImage gi) {
+        this.gi = gi;
+    }
+
     /**
-     * Given the "numbers" map with all the parsed out dicom tag values we
-     * care about..... populate the general image object with these values.
+     * Given the "numbers" map with all the parsed out dicom tag values we care
+     * about..... populate the general image object with these values.
      */
-    private static void populateMRImageFromNumbers(Map numbers, 
-    		                                       MRImage mri) throws Exception {
-		String temp;
-				
-		if ((temp = (String) numbers.get(DicomConstants.IMAGE_TYPE)) != null) {
-			String[] token = temp.split("\\\\");
-	    	if (token.length >= 3){
-	    		mri.setImageTypeValue3(token[2]);
-	    	}
-        }	
+    private static void populateMRImageFromNumbers(Map numbers, MRImage mri)
+            throws Exception {
+        String temp;
 
-    	if ((temp = (String) numbers.get(DicomConstants.SCANNING_SEQUENCE)) != null) {
+        if ((temp = (String) numbers.get(DicomConstants.IMAGE_TYPE)) != null) {
+            String[] token = temp.split("\\\\");
+            if (token.length >= 3) {
+                mri.setImageTypeValue3(token[2]);
+            }
+        }
+
+        if ((temp = (String) numbers.get(DicomConstants.SCANNING_SEQUENCE)) != null) {
             mri.setScanningSequence(temp.trim());
         }
-    	
-    	if ((temp = (String) numbers.get(DicomConstants.SEQUENCE_VARIANT)) != null) {
+
+        if ((temp = (String) numbers.get(DicomConstants.SEQUENCE_VARIANT)) != null) {
             mri.setSequenceVariant(temp.trim());
-        }    	
+        }
 
         if ((temp = (String) numbers.get(DicomConstants.REPETITION_TIME)) != null) {
             mri.setRepetitionTime(Double.valueOf(temp.trim()));
         }
-        
+
         if ((temp = (String) numbers.get(DicomConstants.ECHO_TIME)) != null) {
             mri.setEchoTime(Double.valueOf(temp.trim()));
-        }        
-        
+        }
+
         if ((temp = (String) numbers.get(DicomConstants.INVERSION_TIME)) != null) {
             mri.setInversionTime(Double.valueOf(temp.trim()));
-        } 
-        
+        }
+
         if ((temp = (String) numbers.get(DicomConstants.SEQUENCE_NAME)) != null) {
             mri.setSequenceName(temp.trim());
         }
-        
+
         if ((temp = (String) numbers.get(DicomConstants.IMAGED_NUCLEUS)) != null) {
             mri.setImagedNucleus(temp.trim());
-        }  
-        
-        if ((temp = (String) numbers.get(
-                        DicomConstants.MAGNETIC_FIELD_STRENGTH)) != null) {
+        }
+
+        if ((temp = (String) numbers
+                .get(DicomConstants.MAGNETIC_FIELD_STRENGTH)) != null) {
             mri.setMagneticFieldStrength(Double.valueOf(temp.trim()));
         }
 
         if ((temp = (String) numbers.get(DicomConstants.SAR)) != null) {
-           mri.setSar(Double.valueOf(temp.trim()));
+            mri.setSar(Double.valueOf(temp.trim()));
         }
 
         if ((temp = (String) numbers.get(DicomConstants.DB_DT)) != null) {
@@ -127,5 +129,14 @@ public class MRImageOperation extends DomainOperation implements MRImageOperatio
         if ((temp = (String) numbers.get(DicomConstants.ANGIO_FLAG)) != null) {
             mri.setAngioFlag(temp.trim());
         }
-    }	
+        if (mri.getImageTypeValue3() == null) {
+            throw new Exception("Image Type 3 cannot be null");
+        }
+        if (mri.getScanningSequence() == null) {
+            throw new Exception("Scanning Sequence cannot be null");
+        }
+        if (mri.getSequenceVariant() == null) {
+            throw new Exception("Scanning Variant cannot be null");
+        }
+    }
 }

f33a2d486394b9eb9e203eb23d3fb2cd4d028663
Vrushali Nikte <vrushali.nikte@nih.gov>
2013-04-08 14:46:37 +0000

    Fixed remote download manager to retry download

--- nbia-remote-search/src/gov/nih/nci/nbia/download/RemoteSeriesDownloader.java
+++ nbia-remote-search/src/gov/nih/nci/nbia/download/RemoteSeriesDownloader.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.nbia.download;
 
+import java.sql.Timestamp;
+
 import gov.nih.nci.nbia.remotesearch.RemoteNode;
 import gov.nih.nci.ncia.search.NBIANode;
 import gov.nih.nci.ncia.search.SeriesSearchResult;
@@ -19,7 +21,7 @@ public class RemoteSeriesDownloader extends AbstractSeriesDownloader {
     public void runImpl() throws Exception {
     	//for pause/resume
     	downloaded = 0;
-    	
+    	int retryAttempts =0;
     	RemoteSeriesFileRetriever remoteSeriesFileRetriever = new RemoteSeriesFileRetriever();
     	remoteSeriesFileRetriever.setProgressDelegate(progressUpdater);
     	remoteSeriesFileRetriever.setOutputDirectory(outputDirectory);
@@ -35,14 +37,34 @@ public class RemoteSeriesDownloader extends AbstractSeriesDownloader {
     	//exclusion list? enhancement for later
     	//sop exlucions list for "resume" - enhancement for later
     	try {
-    		remoteSeriesFileRetriever.retrieveImages(seriesSearchResult);
-    		status = COMPLETE;
+    		status = retrieveSeriesImages(remoteSeriesFileRetriever, seriesSearchResult, retryAttempts);
     	}
     	catch(Exception ex) {
     		super.error();
     	}
     }
 
+
+	private int retrieveSeriesImages(RemoteSeriesFileRetriever remoteSeriesFileRetriever,
+			SeriesSearchResult seriesSearchResult, int retryAttempt) throws Exception {
+		if (retryAttempt >= noOfRetry) {
+			additionalInfo.append(getTimeStamp() + " For series " + this.seriesInstanceUid +" Reached max retry (" + noOfRetry + ") attempts.\n");
+			System.out.println(additionalInfo + "--attempt" + retryAttempt +" changing to error status");
+			super.error();
+			return AbstractSeriesDownloader.ERROR;
+		}
+		try {
+			remoteSeriesFileRetriever.retrieveImages(seriesSearchResult);
+    	}
+    	catch(Exception ex) {
+    		additionalInfo.append(getTimeStamp() + ex.getMessage()).append(" retry attempt "+ retryAttempt +".\n");
+    		retrieveSeriesImages(remoteSeriesFileRetriever, seriesSearchResult, retryAttempt+1);
+    	}
+		return AbstractSeriesDownloader.COMPLETE;
+	}
+	private String getTimeStamp () {
+		return new Timestamp(System.currentTimeMillis()).toString();
+	}
 
     /**
      * {@inheritDoc}

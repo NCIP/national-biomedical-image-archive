b17294937a5c85dacbbd2232b579e3e86bdbf559
Vrushali Nikte <vrushali.nikte@nih.gov>
2013-04-08 14:36:03 +0000

    Modified sh to run dicom file size corrector first

--- software/nbia-ctp/conf/dbupdator.sh
+++ software/nbia-ctp/conf/dbupdator.sh
@@ -1,3 +1,3 @@
-java -cp nbiaDatabaseUpdator.jar gov.nih.nci.nbia.util.MRUtil
-sleep 5m
 java -cp nbiaDatabaseUpdator.jar gov.nih.nci.nbia.util.DicomFileSizeCorrectorUtil
+sleep 5m
+java -cp nbiaDatabaseUpdator.jar gov.nih.nci.nbia.util.MRUtil

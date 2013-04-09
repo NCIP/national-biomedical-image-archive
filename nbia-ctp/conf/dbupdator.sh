36773781a0b368ec444d2facd9ef87a7d13407b0
Vrushali Nikte <vrushali.nikte@nih.gov>
2013-04-09 18:02:20 +0000

    removed invisible window characters

--- nbia-ctp/conf/dbupdator.sh
+++ nbia-ctp/conf/dbupdator.sh
@@ -1,3 +1,7 @@
-java -cp nbiaDatabaseUpdator.jar gov.nih.nci.nbia.util.DicomFileSizeCorrectorUtil
-sleep 5m
-java -cp nbiaDatabaseUpdator.jar gov.nih.nci.nbia.util.MRUtil
+export PATH=/usr/local/jboss-5.1.0.GA/server/ncicb-29080/DBUpdater:$PATH
+export CLASSPATH=$CLASSPATH:${PWD}/nbiaDatabaseUpdator.jar
+echo $CLASSPATH
+echo $PATH
+java -cp nbiaDatabaseUpdator.jar gov.nih.nci.nbia.util.DicomFileSizeCorrectorUtil 
+echo starting another process
+java -cp nbiaDatabaseUpdator.jar gov.nih.nci.nbia.util.MRUtil 

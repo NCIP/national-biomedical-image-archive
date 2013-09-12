8302cbdc180ec7f09441ce4a871bccb01936f29d
vrushalinikte <vrushali.nikte@nih.gov>
2013-09-12 12:14:28 -0400

    Fixed NBIA-653	Clicking on View My Basket gives NBIA error after User adds Images to Basket removing unused import

--- software/nbia-dao/src/gov/nih/nci/nbia/dao/StudyDAOImpl.java
+++ software/nbia-dao/src/gov/nih/nci/nbia/dao/StudyDAOImpl.java
@@ -18,7 +18,6 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 

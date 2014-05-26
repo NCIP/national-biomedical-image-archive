ad933a6509e2aa12f000de6e70d6c7e48899d663
sgustaf <scott.gustafson@ELLARCH1.ellumen.local>
2014-05-26 14:05:40 -0400

    Should not have deleted

---
+++ software/nbia-dao/src/gov/nih/nci/nbia/dao/WorkflowDAO.java
@@ -0,0 +1,32 @@
+/*L
+ *  Copyright SAIC, Ellumen and RSNA (CTP)
+ *
+ *
+ *  Distributed under the OSI-approved BSD 3-Clause License.
+ *  See http://ncip.github.com/national-biomedical-image-archive/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.nbia.dao;
+
+import java.util.List;
+
+import gov.nih.nci.nbia.dto.WorkflowDTO;
+import gov.nih.nci.nbia.internaldomain.Workflow;
+
+
+import org.springframework.dao.DataAccessException;
+
+public interface WorkflowDAO {
+	 
+
+	public List<WorkflowDTO> getNewSeriesWorkflowsByCollectionAndSite(String collection, String site) throws DataAccessException;
+	
+	public List<WorkflowDTO> getVisibilityWorkflowsByCollectionAndSite(String collection, String site) throws DataAccessException;
+	
+	public WorkflowDTO getWorkflowById(Integer wid) throws DataAccessException;
+	
+	public List<WorkflowDTO> getWorkflows() throws DataAccessException ;
+	
+	public long delete(WorkflowDTO toRemove) throws DataAccessException ;
+	
+}

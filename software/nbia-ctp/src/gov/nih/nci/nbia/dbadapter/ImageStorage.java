b7f204da65514fc1ffa5d7f95ef56eb189a46453
sgustaf <sgustafson@ellumen.com>
2015-09-03 08:16:23 -0400

    Fixing modalities

--- software/nbia-ctp/src/gov/nih/nci/nbia/dbadapter/ImageStorage.java
+++ software/nbia-ctp/src/gov/nih/nci/nbia/dbadapter/ImageStorage.java
@@ -157,7 +157,7 @@ public class ImageStorage extends HibernateDaoSupport{
             return Status.FAIL;
         }
 
-        if (numbers.get(DicomConstants.MODALITY).equals("MR")) {        
+        if (numbers.get(DicomConstants.SOP_CLASS_UID).toString().startsWith("1.2.840.10008.5.1.4.1.1.4")) {        
 	        try {
 				mrio.setGeneralImage(gi);
 				MRImage mr = (MRImage)mrio.validate(numbers);

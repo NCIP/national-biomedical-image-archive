3186e237eafbd5d040338ae813efc8ecc307d96c
sgustaf <sgustafson@ellumen.com>
2015-09-09 06:12:26 -0400

    fix for slowness

---
+++ software/nbia-api/src/gov/nih/nci/nbia/restUtil/CollectionUtil.java
@@ -0,0 +1,23 @@
+package gov.nih.nci.nbia.restUtil;
+import gov.nih.nci.nbia.restAPI.String;
+
+import java.util.List;
+public class CollectionUtil {
+
+	private long created;
+	List<String> authorizedCollections;
+	public long getCreated() {
+		return created;
+	}
+	public void setCreated(long created) {
+		this.created = created;
+	}
+	public List<String> getAuthorizedCollections() {
+		return authorizedCollections;
+	}
+	public void setAuthorizedCollections(List<String> authorizedCollections) {
+		this.authorizedCollections = authorizedCollections;
+	}
+	
+	
+}

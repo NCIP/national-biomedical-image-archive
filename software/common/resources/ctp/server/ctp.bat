4363cec8318bb5585bb7a7490366772c7a84c2d3
panq <panq@mail.nih.gov>
2016-04-20 22:11:43 -0400

    Upgrade CTP to X67.

--- software/common/resources/ctp/server/ctp.bat
+++ software/common/resources/ctp/server/ctp.bat
@@ -1,6 +1,4 @@
-
-        
 echo Starting CTP Server...
-java -Xmx1024m -cp xml-apis-1.0.b2.jar -Djava.ext.dirs="../jai_imageio-1_1/lib/;%JAVA_HOME%/jre/lib/ext" -jar Runner.jar
+java -Xmx1024m -cp xml-apis-1.0.b2.jar -Djava.ext.dirs="%JAVA_HOME%/jre/lib/ext" -jar Runner.jar
         
         

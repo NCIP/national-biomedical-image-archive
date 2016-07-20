8342f9b7e8c1ea4227f799671fb099ce5b1a65b1
panq <panq@mail.nih.gov>
2016-07-20 09:56:15 -0400

    Remove modal attribute for dialog which may mess up with the editable field in drop down.

--- software/nbia-uat/app/conf/globals.ts
+++ software/nbia-uat/app/conf/globals.ts
@@ -1,8 +1,7 @@
 //
 // global constants
 //
-export var sep='/';
-export var version: string="3";
+
 //export var serviceUrl: string="http://ncias-d1342-v.nci.nih.gov:8080/nbia-api/services/v3/";  
 export var serviceUrl: string="http://localhost:8080/nbia-api/services/v3/";  
 export var accessToken;
